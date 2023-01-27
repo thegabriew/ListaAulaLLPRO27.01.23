351bb1fa99a93391d98ef3330833dd97f43c62d6
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-01-27 16:41:14 -0300

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,25 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<locale.h>
 
-int main (){
+int main() {
 	
-	int i=0,num[3],maior,menor,cont;
-	maior = num[0];
-	menor = num[0];
-	
-	for ( i=0; i<3; i++){
-		
-		scanf("%d",&num[3]);
-		
-			if(maior<=num[i]){
-		
-		maior = num[i];
-		menor = num [i-1];
-		
-		printf("O maior eh o %d ",num[i]);
-		printf("O maior eh o %d ",num[i]);
-	}
+    int num[10];
+    int i, maior, menor,maiorIn,menorIn;
 
+	setlocale(LC_ALL,"Portuguese_Brazil");
 
-}
+    for (i=0; i<10; i++) {
+        printf("Digite um número: ");
+        scanf("%d", &num[i]);
+    }
+
+    maior = num[0];
+    menor = num[0];
+
+    for (i = 0; i < 10; i++) {
+    	
+        if (num[i]>maior) {
+            maior = num[i];
+            maiorIn = i;
+          
+        }
+        if (num[i]<menor) {
+            menor = num[i];
+            menorIn = i;
+    
+        }
+    }
+
+    printf("\nO menor valor é %d e seu índice é %d e o maior é %d e seu índice é %d \n",menor,menorIn,maior,maiorIn);
+
+    return 0;
 }
