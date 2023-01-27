075e692d3e6089f79947a5fcbee7a482f149e539
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-01-27 16:21:10 -0300

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main (){
+	
+	int i=0,num[3],maior,menor,cont;
+	maior = num[0];
+	menor = num[0];
+	
+	for ( i=0; i<3; i++){
+		
+		scanf("%d",&num[3]);
+		
+			if(maior<=num[i]){
+		
+		maior = num[i];
+		menor = num [i-1];
+		
+		printf("O maior eh o %d ",num[i]);
+		printf("O maior eh o %d ",num[i]);
+	}
+
+
+}
+}
